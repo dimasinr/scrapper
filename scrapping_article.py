b7a08659ff29ad554c2f36b0164df6708fb2fb08
dimas indra <dimas.indra773@gmail.com>
2023-07-30 20:42:47 +0700

    add row image

--- scrapping_article.py
+++ scrapping_article.py
@@ -26,6 +26,18 @@ def scraping(url):
         print("Error : ", e)
         return None
 
+def imageregx(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    div_element = soup.find("div", class_="bg-cover 3xl:bg-auto bg-center bg-no-repeat relative")
+    if div_element:
+        style_attr = div_element.get("style")
+        if style_attr:
+            match = re.search(r"url\('([^']+)'\)", style_attr)
+            if match:
+                return match.group(1)
+
+    return None
+
 def excelsave(contents, file_name):
     workbook = openpyxl.Workbook()
     sheet = workbook.active
@@ -36,19 +48,19 @@ def excelsave(contents, file_name):
         # Find the specific div element and extract its text
         div_element = soup.find("div", class_="sm:text-sm md:text-2xl lg:text-5xl font-bold")
         if div_element:
-            div_text = div_element.text.strip()
+            title_text = div_element.text.strip()
         else:
-            div_text = "No text found in the div element."
+            title_text = "tidak ada teks"
+
+        image_url = imageregx(content)
 
         sheet.cell(row=idx, column=1, value=content)
-        sheet.cell(row=idx, column=2, value=div_text)
+        sheet.cell(row=idx, column=2, value=title_text)
+        sheet.cell(row=idx, column=3, value=image_url)
+        print(f"judul {title_text}", f"gambar : {image_url}")
 
     workbook.save(file_name)
     print("Data berhasil disimpan : ", file_name)
-
-def cleanparser(html_text):
-    clean_html = re.sub(r'(?<=>)\"(?=<)', '', html_text)
-    return clean_html
 
 urls = geturl()
 print(urls)
@@ -60,6 +72,6 @@ for url in urls:
         isi_content.append(content)
 
 if isi_content:
-    excelsave(isi_content, "coba lgi zzzz.xlsx")
+    excelsave(isi_content, "coba lgi wawawa.xlsx")
 else:
     print("Gagal scraping.")
